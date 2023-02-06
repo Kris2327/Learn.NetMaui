12d408a52bb51bb53682c05cda98f1c102ce8313
Kris2327 <117087003+Kris2327@users.noreply.github.com>
2023-02-06 10:12:47 +0200

    konche

---
+++ App/Learn .NET MAUI/Fundaments.xaml.cs
@@ -0,0 +1,36 @@
+using Learn_.NET_MAUI.Fundamentals;
+
+namespace Learn_.NET_MAUI;
+
+public partial class Fundaments : ContentPage
+{
+    public Fundaments()
+    {
+        InitializeComponent();
+    }
+
+    private async void TemplatesBtn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new Templates_Fundamentals());
+    }
+
+    private async void DataBindingBtn_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new DataBinding_Fundamentals());
+    }
+
+    private void FistStepsBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+        await Navigation.PushAsync(new DataSaving());
+	}
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+}
